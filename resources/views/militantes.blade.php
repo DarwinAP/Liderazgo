e2e024e88a5ccf2f17d197eb013de3cc5e1558e6
DarwinAP <darwin199028@hotmail.com>
2020-06-04 19:59:06 -0500

    vistas y rutas

---
+++ resources/views/militantes.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+         <meta charset="utf8">
+         <title>DARWIN</title>
+    </head>
+    <body>
+        <h1>Darwin Alvarado</h1>
+    </body>
+</html>
